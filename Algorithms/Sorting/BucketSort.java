846b4a9813fd155c2a549e3d600ac11cc9a2ee5f
musheer <musheer93@gmail.com>
2016-08-21 02:30:20 +0530

    Check

--- Algorithms/Sorting/BucketSort.java
+++ Algorithms/Sorting/BucketSort.java
@@ -1,9 +1,10 @@
 public class BucketSort {
 
    // Algorithm -> This sort works if the array elements are within a range.
+   //           -> This gets this name because we are sorting the elements(using Insertion Sort) inside the Bucket.
    //    Steps :
    //         -> We will write a hash function that will decide in which Bucket the element will go.
    //         -> Then we sort the elements in each Bucket using Insertion Sort.
    //         -> The we will take elements from each Bucket(in the same order) and insert it back into the Array.
-
+   //
 }
