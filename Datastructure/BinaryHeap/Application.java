a710d158103828de234c26dffe8d795bd4eb90fa
musheer <musheer93@gmail.com>
2016-08-27 23:22:32 +0530

    Heap Sort BUBBLE Down Technique

---
+++ Datastructure/BinaryHeap/Application.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Application {
+
+   public static void main(String[] args) {
+      int[] arr = new int[12];
+
+      arr[0] = 2;
+      arr[1] = 8;
+      arr[2] = 10;
+      arr[3] = 3;
+      arr[4] = 15;
+      arr[5] = 6;
+      arr[6] = 30;
+      arr[7] = 19;
+      arr[8] = 32;
+      arr[9] = 4;
+      arr[10] = 11;
+      arr[11] = 0;
+
+      heapSort(arr);
+   }
+
+   /*
+     Build the Heap and then DESTROY it(call delete n times)
+   */
+   public static void heapSort(int[] arr) {
+
+      Heap heap = buildHeap(arr);
+      for(int i=heap.getCount();i>0;i--) {
+         heap.delete();
+      }
+      System.out.println("-------------------");
+      System.out.println(Arrays.toString(heap.getArr()));
+   }
+
+   public static Heap buildHeap(int[] arr) {
+      Heap heap = new Heap(20, 0);
+      heap.setArr(arr);
+      heap.setCount(arr.length);
+      int lastNonLeaf = ((heap.getCount()-1)-1)/2;
+      for(int i=lastNonLeaf;i>=0;i--) {
+         heap.bubbleDown(i);
+      }
+      System.out.println(Arrays.toString(arr));
+      return heap;
+   }
+
+   public static void swap(int[] arr, int index1, int index2) {
+      int temp = arr[index1];
+      arr[index1] = arr[index2];
+      arr[index2] = temp;
+   }
+
+}
---
+++ Datastructure/BinaryHeap/Heap.java
@@ -0,0 +1,99 @@
+public class Heap {
+
+   private int[] arr;
+   private int count;
+   private int capacity;
+   private int heapType;
+
+   public Heap(int capacity, int heapType) {
+      this.capacity = capacity;
+      this.heapType = heapType;
+      this.arr = new int[capacity];
+   }
+
+   public int[] getArr() {
+      return this.arr;
+   }
+
+   public void setArr(int[] arr) {
+      this.arr = arr;
+   }
+
+   public int getCount() {
+      return this.count;
+   }
+
+   public void setCount(int count) {
+      this.count = count;
+   }
+
+   public void incrementCount() {
+      this.count++;
+   }
+
+   public void descrementCount() {
+      this.count--;
+   }
+
+   public int getLChild(int parent) {
+      int lChild = parent*2+1;
+      if(lChild >= count) {
+         return -1;
+      }
+      return lChild;
+   }
+
+   public int getRChild(int parent) {
+      int rChild = parent*2+2;
+      if(rChild >= count) {
+         return -1;
+      }
+      return rChild;
+   }
+
+   public void delete() {
+      if(count <= 0) {
+         return;
+      }
+      Application.swap(this.arr, 0, this.count-1);
+      descrementCount();
+      // Heapifying
+      int parent = 0;
+      bubbleDown(parent);
+
+   }
+
+   public void bubbleDown(int parent) {
+      int lChild = getLChild(parent);
+      int rChild = getRChild(parent);
+      while(lChild != -1 || rChild != -1) {
+         int minIndex = parent;
+         if(rChild != -1 && arr[rChild] < arr[minIndex]) {
+            minIndex = rChild;
+         }
+         if(lChild != -1 && arr[lChild] < arr[minIndex]) {
+            minIndex = lChild;
+         }
+         if(minIndex == parent) {
+            break;
+         }
+         Application.swap(arr, parent, minIndex);                 // The while condition makes sure that the parent is more than atleast one of its children
+         parent = minIndex;
+         rChild = getRChild(parent);
+         lChild = getLChild(parent);
+      }
+   }
+
+   public void insert(int data) {
+      this.arr[this.count] = data;
+      int child = this.count;
+      this.count++;
+      int parent = (child-1)/2;
+      while(parent >= 0 && arr[parent] > data) {
+         Application.swap(this.arr, parent, child);
+         child = parent;
+         parent = (child-1)/2;
+      }
+   }
+
+}
