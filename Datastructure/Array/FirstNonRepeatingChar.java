621d76654d8ab7330508e9568eaab0fd70029ca6
musheer <musheer93@gmail.com>
2016-12-06 19:20:26 +0530

    s

---
+++ Datastructure/Array/FirstNonRepeatingChar.java
@@ -0,0 +1,11 @@
+public class FirstNonRepeatingChar {
+
+   // 1 -> Use Two Loops                                            - O(n^2)
+   // 2 -> Use a Count Array to Store Count of Each Element         -  O(n)
+   // 3 -> Use a Count Array, but for the First Occurence alone,    -  O(n)   Preffered
+   //      store the char Index in the String as Value(i.e Not the Count).
+
+
+
+
+}
