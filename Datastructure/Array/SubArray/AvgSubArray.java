46e4c8feb2fe9132eb53ee844100f4792fc24664
musheer <musheer93@gmail.com>
2016-12-06 22:16:01 +0530

    s

--- Datastructure/Array/SubArray/AvgSubArray.java
+++ Datastructure/Array/SubArray/AvgSubArray.java
@@ -1,4 +1,4 @@
-public class MaximumAvgSubArray {
+public class AvgSubArray {
 
    // Given an Array and a Number k, print the Maximum Average Sub-Array of Size k.
    // Maximum Average Sub-Array of Sise k is a Sub-Array for which the Average is Maximum among all Possible Sub-Array of Size k in the Array.
@@ -6,6 +6,8 @@ public class MaximumAvgSubArray {
 
    // 1 -> Sliding Window Stratergy. Calculate the Sum for Each Window and update the Max
 
+   // Note :
+   //    -> The same can be done for subarray with least average
 
    public static void maxAverage(int[] a, int k) {
 
@@ -23,6 +25,27 @@ public class MaximumAvgSubArray {
             maxSum = currSum;
             maxStart = i;
             maxEnd = j;
+         }
+      }
+      System.out.println(maxStart+" "+maxEnd+" "+maxSum);
+   }
+
+   public static void minAverage(int[] a, int k) {
+
+      int currSum = 0;
+      int maxSum = -1;
+      int minStart = 0, minEnd = k-1;
+      for(int i=0;i<k;i++) {
+         currSum += a[i];
+      }
+      minSum = currSum;                                // if Average is required, then divide the Sum by k
+      for(int i=1, j=k;j<a.length;i++, j++) {
+         currSum -= a[i-1];
+         currSum += a[j];
+         if(currSum < minSum) {
+            minSum = currSum;
+            minStart = i;
+            minEnd = j;
          }
       }
       System.out.println(maxStart+" "+maxEnd+" "+maxSum);
